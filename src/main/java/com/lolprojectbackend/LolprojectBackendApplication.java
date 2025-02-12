45c8f69cf445eb3c412aca5ebad3faaa7ac5340f
Hongse0 <hongseyoung981007@gmail.com>
2025-02-12 17:21:34 +0900

    Initial commit

---
+++ src/main/java/com/lolprojectbackend/LolprojectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lolprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolprojectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolprojectBackendApplication.class, args);
+    }
+
+}
